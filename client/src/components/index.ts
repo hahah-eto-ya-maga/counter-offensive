9a2898a8f77fb8ac66f7cfde40f168d59fec234e
ester kinsky <esterkinsky@mail.ru>
2023-10-15 01:47:54 +0400

    registration form

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,6 +1,7 @@
 import ProgressBar from "./ProgressBar/ProgressBar";
 import CardAdvise from "./CardAdvice/CardAdvice";
 import Login from "./Login/Login";
+import Registration from "./Registration/Registration";
 
-export { ProgressBar, CardAdvise, Login };
+export { ProgressBar, CardAdvise, Login, Registration };
 export { Button, Logo, Input } from "./UI";
