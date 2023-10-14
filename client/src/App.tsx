55cf565b8a1f85c63a0960487196f09c24055708
ester kinsky <esterkinsky@mail.ru>
2023-10-14 22:36:19 +0400

    made login form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import "./styles/global.css";
+import { Login } from './components';
 
 const App: React.FC = () => {
-   return <div className="app"></div>;
+   return <div className="app">
+	<Login/>
+   </div>;
 };
 
 export default App;
--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,5 +1,6 @@
 import ProgressBar from "./ProgressBar/ProgressBar";
 import CardAdvise from "./CardAdvice/CardAdvice";
+import Login from "./Login/Login";
 
-export { ProgressBar, CardAdvise };
+export { ProgressBar, CardAdvise, Login };
 export { Button, Logo, Input } from "./UI";
