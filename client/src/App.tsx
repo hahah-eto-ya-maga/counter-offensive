3c884c8389ad08cc909394d324a93c23bae5045f
ek5em <pro-100-dim@mail.ru>
2023-10-15 18:32:33 +0400

    [ADD] Добавлена компонента кнопки.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import "./styles/global.css";
-import { LoadingPage } from "./pages";
+import Button from "./components/UI/Button/Button";
 
 const App: React.FC = () => {
-   return (
-      <div className="app">
-         <LoadingPage />
+   return <div className="app">
+      <div>
+         <Button appearance="menu">Для меню</Button>
+         <Button appearance="primary">Обычная</Button>
+         <Button appearance="primary-disable">Отключена</Button>
       </div>
-   );
+   </div>;
 };
 
 export default App;
