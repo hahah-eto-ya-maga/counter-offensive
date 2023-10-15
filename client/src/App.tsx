619fb7c6e33b31f300d9d78135708a1cf8de50c4
ester kinsky <esterkinsky@mail.ru>
2023-10-15 17:14:11 +0400

    ready

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,9 @@
 import React from "react";
 import "./styles/global.css";
-import { LoadingPage } from "./pages";
-import { Login } from './components';
 
 const App: React.FC = () => {
    return (
       <div className="app">
-         <Login />
       </div>
    );
 };
