68fd00563d316c378bed184f4c6f4d4da0c4e3c1
ester kinsky <esterkinsky@mail.ru>
2023-10-15 16:49:58 +0400

    fixed input padding

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./styles/global.css";
 import { LoadingPage } from "./pages";
+import { Login } from './components';
 
 const App: React.FC = () => {
    return (
       <div className="app">
-         <LoadingPage />
+         <Login />
       </div>
    );
 };
