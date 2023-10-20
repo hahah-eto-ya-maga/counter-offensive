3153e505eb07782661a480077e1b744c91ea4fb7
Shi-Geo <shihaleev.georgyalex@gmail.com>
2023-10-20 20:05:43 +0400

    незначительные изменения

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import "./styles/global.css";
-import { LobbiPage } from "./pages";
 
 const App: React.FC = () => {
   return (
     <div className="app">
-      <LobbiPage/>
     </div>
   );
 };
