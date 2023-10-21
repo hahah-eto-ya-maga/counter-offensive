96e71f55e48ffb34ae1c7dfdc9eb8e7778c6cdcd
ester kinsky <esterkinsky@mail.ru>
2023-10-21 23:47:41 +0400

    add menuPage, add it to PH

--- client/src/pages/PageHandler/PageHandler.tsx
+++ client/src/pages/PageHandler/PageHandler.tsx
@@ -5,44 +5,43 @@ import LoadingPage from "../LoadingPage/LoadingPage";
 import DossierPage from "../DossierPage/DossierPage";
 
 import "./PageHandler.css";
+import MenuPage from "../MenuPage/MenuPage";
 
-type TPage = "Lobbi" | "Dossier" | "Loading" | "Menu";
+export type TPage = "Lobby" | "Dossier" | "Loading" | "Menu";
 
 const PageHandler: React.FC = () => {
-   const [page, setPage] = useState<TPage>("Lobbi");
-   return (
-      <div className="page_handler">
-         <div className="header">
-            <Button
-               appearance="primary"
-               active={page === "Lobbi"}
-               onClick={() => setPage("Lobbi")}
-            >
-               Лобби
-            </Button>
-            <Button
-               appearance="primary"
-               active={page === "Dossier"}
-               onClick={() => setPage("Dossier")}
-            >
-               Досье
-            </Button>
-            <Button
-               appearance="primary"
-               active={page === "Menu"}
-               onClick={() => setPage("Menu")}
-            >
-               Меню
-            </Button>
-         </div>
-         <>
-            {page === "Dossier" && <DossierPage />}
-            {page === "Loading" && <LoadingPage />}
-            {page === "Lobbi" && <LobbiPage />}
-            {page === "Menu"}
-         </>
-      </div>
-   );
+  const [page, setPage] = useState<TPage>("Lobby");
+  return (
+    <div className="page_handler">
+      {page !== "Menu" && (
+        <div className="header">
+          <Button
+            appearance="primary"
+            active={page === "Lobby"}
+            onClick={() => setPage("Lobby")}
+          >
+            Лобби
+          </Button>
+          <Button
+            appearance="primary"
+            active={page === "Dossier"}
+            onClick={() => setPage("Dossier")}
+          >
+            Досье
+          </Button>
+          <Button appearance="primary" onClick={() => setPage("Menu")}>
+            Меню
+          </Button>
+        </div>
+      )}
+      <>
+        {page === "Dossier" && <DossierPage />}
+        {page === "Loading" && <LoadingPage />}
+        {page === "Lobby" && <LobbiPage />}
+        {page === "Menu" && <MenuPage goToLobby={ setPage} />}
+      </>
+    </div>
+  );
 };
 
 export default PageHandler;
--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -2,5 +2,6 @@ import DossierPage from "./DossierPage/DossierPage";
 import LoadingPage from "./LoadingPage/LoadingPage";
 import LobbiPage from "./LobbiPage/LobbiPage";
 import PageHandler from "./PageHandler/PageHandler";
+import MenuPage from "./MenuPage/MenuPage";
 
-export { LoadingPage, DossierPage, LobbiPage, PageHandler };
+export { LoadingPage, DossierPage, LobbiPage, PageHandler, MenuPage };
