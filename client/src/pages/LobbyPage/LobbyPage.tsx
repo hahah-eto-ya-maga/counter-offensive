bc9485f16bdfd76320da271a4c962771d91841cd
Shi-Geo <shihaleev.georgyalex@gmail.com>
2023-10-26 23:13:24 +0400

    [FIX] Lobbi

--- client/src/pages/LobbyPage/LobbyPage.tsx
+++ client/src/pages/LobbyPage/LobbyPage.tsx
@@ -3,6 +3,7 @@ import { Button } from "../../components"
 import { automat, RPG, tank2, tank3 } from "./images"; 
 import { General, FlagBearer } from "../../components/Lobby";
 import "./LobbyPage.css";
+import DossierPage from "../DossierPage/DossierPage";
 
 const LobbyPage: React.FC = () => {
     
@@ -17,6 +18,9 @@ const LobbyPage: React.FC = () => {
                     <General/>
                     <Button className="automat" appearance="image" onClick={() => {}}> Пехотинец-автоматчик <img src={automat} alt="Automat" /></Button>
                     <Button className="tank3" appearance="image" onClick={() => {}}> Трёхместный танк <img src={tank3} alt="Tank_3" /> </Button>
+                </div>
+                <div className="dossier">
+                    <DossierPage/>
                 </div>
             </div>
     )
