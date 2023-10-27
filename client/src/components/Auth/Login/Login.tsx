4d4d9463a92d30ea6f1d58bb29b045497c6c23a1
Y4sper <66819081+hahah-eto-ya-maga@users.noreply.github.com>
2023-10-27 18:07:34 +0400

    fix pulling

--- client/src/components/Auth/Login/Login.tsx
+++ client/src/components/Auth/Login/Login.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import { Button, Input } from "../../UI";
 import { IUserData } from "../../../interfaces";
-import "./Login.css";
+import "../../../pages/MainPage/MainPage.css";
+import { ISetPage } from '../../../interfaces';
 
-const Login: React.FC = () => {
+const Login: React.FC<ISetPage> = ({ setPage }) => {
   const [userData, setUserData] = useState<IUserData>({
     login: "",
     password: "",
@@ -16,34 +17,40 @@ const Login: React.FC = () => {
   const onSubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {};
 
   return (
-    <form className="auth_form" onSubmit={onSubmitHandler}>
-      <div className="row_div">
-        <div className="column_div">
-          <Input
-            text="Логин"
-            value={userData.login}
-            onChange={(value) => {
-              onChangeHandler(value, "login");
-            }}
-          />
-          <Input
-            text="Пароль"
-            type="password"
-            value={userData.password}
-            onChange={(value) => {
-              onChangeHandler(value, "password");
-            }}
-          />
+    <form className="main_form" onSubmit={onSubmitHandler}>
+      <div>
+        <Input
+          text="Логин"
+          value={userData.login}
+          onChange={(value) => {
+            onChangeHandler(value, "login");
+          }}
+        />
+        <Input
+          text="Пароль"
+          type="hidePassword"
+          value={userData.password}
+          onChange={(value) => {
+            onChangeHandler(value, "password");
+          }}
+        />
+      </div>
+      <div className="errors_div">
+        <div className="warning">
+          <span>Заполните все поля</span>
+        </div>
+        <div className="error">
+          <span>Неверный логин или пароль</span>
         </div>
       </div>
-      <div className="row_div">
-        <div className="column_div">
-          <div className="error_div">Неверный логин или пароль</div>
-          <div className="warning_div">Заполните все поля</div>
-        </div>
-      </div>
-      <div className="auth_footer">
-        <Button appearance="primary" className="auth_submit_button">
+      <div className="main_footer">
+        <Button
+          appearance="primary"
+          className="main_submit_button"
+          onClick={() => {
+            setPage("Lobby");
+          }}
+        >
           Пойти на Бахмут
         </Button>
       </div>
