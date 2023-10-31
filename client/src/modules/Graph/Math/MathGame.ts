9bf3e821514c857287704479c120d00cde07f1a9
Shi-Geo <shihaleev.georgyalex@gmail.com>
2023-10-31 20:57:11 +0400

    Переделана граница

--- client/src/modules/Graph/Math/MathGame.ts
+++ client/src/modules/Graph/Math/MathGame.ts
@@ -19,7 +19,28 @@ class MathGame {
         return {x: x, y: y}
     }
 
-    
+    scalarProd(a:TPoint, b:TPoint): number {
+        return a.x * b.x + a.y * b.y;
+    }
+
+
+    multyBlock(polygon: TPoint[]): TPoint[][] {
+        const bloksArray: TPoint[][] = []
+        let pointLeftDown: TPoint = {x:0,y:0} 
+        let pointRightUp: TPoint = {x:0,y:0}
+        for (let i = 0; i < polygon[2].x; i++) {
+            for (let j = 0; j < polygon[2].y; j++) {
+                pointLeftDown = {x:0, y:0}
+                pointRightUp = {x:0, y:0}
+                pointLeftDown.x = polygon[0].x + i
+                pointRightUp.x = pointLeftDown.x + 1
+                pointLeftDown.y = polygon[0].y + j
+                pointRightUp.y = pointLeftDown.y + 1
+                bloksArray.push([pointLeftDown, pointLeftDown, pointRightUp, pointRightUp])
+            }
+        }
+        return bloksArray
+    }
 
 }
 
--- client/src/pages/GamePage/GamePage.tsx
+++ client/src/pages/GamePage/GamePage.tsx
@@ -21,24 +21,9 @@ const GamePage: React.FC = () => {
 
     const keyPressed: TKeyboard = {}
 
-    let angleOfMovement = Math.PI/2;
-    const speedRotate = Math.PI/256;
-    const speedTank = 1/16;
-    let speedTankNow = speedTank;
-    const vectorTank: TPoint = {x: 0, y: 1}
-    
-    const speedInfantry = 1/16;
-    let speedInfantryNow = speedInfantry
-
-    const borderScena: TPoint[] = [{x: 0, y: 0}, {x: 75, y: 0}, {x: 75, y: 60}, {x: 0, y: 60}] 
-    const blocksArray: TPoint[][] = [[{x:6, y: 6}, {x:6, y: 7}, {x:7, y: 7}, {x:7, y: 6}], [{x:6, y: 9}, {x:6, y: 10}, {x:7, y: 10}, {x:7, y: 9}], 
-        [{x:8, y: 9}, {x:8, y: 10}, {x:9, y: 10}, {x:9, y: 9}], [{x:7, y: 8}, {x:7, y: 9}, {x:8, y: 9}, {x:8, y: 8}]]
-
-    const man: TUnit = {x: 7, y: 3, r: 0.3}
+    const man: TUnit = {x: 7, y: 3, r: 0.2}
     const tankU: TUnit = {x: 4, y: 5, r:0.4}
     const tank: TPoint[] = [{x:-0.4, y:- 0.4}, {x:-0.4, y: 0.4}, {x:0.4, y: 0.4}, {x:0.4, y: -0.4}] 
-
-    let collition: boolean = false
 
     const WIN = {
         left: -8 * prop + man.x,
@@ -48,6 +33,24 @@ const GamePage: React.FC = () => {
     };
 
     const math = new MathGame({WIN})
+
+    let angleOfMovement = Math.PI/2;
+    const speedRotate = Math.PI/256;
+    const speedTank = 1/16;
+    let speedTankNow = speedTank;
+    const vectorTank: TPoint = {x: 0, y: 1}
+    
+    const speedInfantry = 1/16;
+    let speedInfantryNow = speedInfantry
+
+    const borderScena: TPoint[] = [{x: 1, y: 1}, {x: 74, y: 1}, {x: 74, y: 59}, {x: 1, y: 59}] 
+
+    const blocksArray: TPoint[][] = [[{x:6, y: 6}, {x:6, y: 7}, {x:7, y: 7}, {x:7, y: 6}], [{x:6, y: 9}, {x:6, y: 10}, {x:7, y: 10}, {x:7, y: 9}], 
+        [{x:8, y: 9}, {x:8, y: 10}, {x:9, y: 10}, {x:9, y: 9}], [{x:7, y: 8}, {x:7, y: 9}, {x:8, y: 9}, {x:8, y: 8}],  [{x:7, y: 7}, {x:7, y: 8}, {x:8, y: 8}, {x:8, y: 7}],
+        ...math.multyBlock([{x: 0, y: 0}, {x: 1, y: 0}, {x: 1, y: 60}, {x: 0, y: 60}]),// ф-я постороения неск блоков
+        ...math.multyBlock([{x: 1, y: 0}, {x: 1, y: 1}, {x: 75, y: 1}, {x: 75, y: 0}])] 
+
+    let collition: boolean = false
 
     useEffect(() => {
         canvas = Canvas({
@@ -125,24 +128,65 @@ const GamePage: React.FC = () => {
     }
 
     const checkBlockInfantry = (block: TPoint[]) => {
-        let nearX = Math.max(block[0].x, Math.min(man.x, block[2].x));
-        let nearY = Math.max(block[0].y, Math.min(man.y, block[2].y));
+        let nearX = Math.max(block[0].x, Math.min(man.x, block[0].x + 1));
+        let nearY = Math.max(block[0].y, Math.min(man.y, block[0].y + 1));
         const nearVector: TPoint = {x: nearX - man.x, y: nearY - man.y}
-        let lengthVector = Math.sqrt(nearVector.x * nearVector.x + nearVector.y * nearVector.y)
-        let overlap = man.r - lengthVector
+        let distance = Math.sqrt(math.scalarProd(nearVector, nearVector))
+        let direction: TPoint = {x: nearVector.x / distance, y: nearVector.y / distance}
+        let overlap = man.r - distance
         if (overlap > 0) {
-            WIN.left = WIN.left - nearVector.x / lengthVector * speedInfantryNow
-            WIN.bottom = WIN.bottom - nearVector.y / lengthVector * speedInfantryNow
+            WIN.left = WIN.left - direction.x * overlap
+            WIN.bottom = WIN.bottom - direction.y * overlap
         }
     }
 
+    const checkCircleInfantry = (block: TUnit) => {
+        let nearX = block.x//Math.max(block[0].x, Math.min(man.x, block[0].x + 1));
+        let nearY = block.y//Math.max(block[0].y, Math.min(man.y, block[0].y + 1));
+        const nearVector: TPoint = {x: nearX - man.x, y: nearY - man.y}
+        let rsum = man.r + block.r  
+        let distance = Math.sqrt(math.scalarProd(nearVector, nearVector))
+        let direction: TPoint = {x: nearVector.x / distance, y: nearVector.y / distance}
+        let overlap = distance - rsum
+        if (overlap < 0) {
+            WIN.left = WIN.left + overlap * (nearX - man.x) / distance
+            WIN.bottom = WIN.bottom + overlap * (nearY - man.y) / distance
+        }
+    }
+
+    const checkNearBlock = (block: TPoint[], index: number): {length: number, vector: TPoint} => {
+        let nearX = Math.max(block[0].x, Math.min(man.x, block[0].x + 1));
+        let nearY = Math.max(block[0].y, Math.min(man.y, block[0].y + 1));
+        let nearVector: TPoint = {x: nearX - man.x, y: nearY - man.y}
+        let lengthVector = Math.sqrt(nearVector.x * nearVector.x + nearVector.y * nearVector.y)
+        return {length: lengthVector, vector: nearVector}
+        // let overlap = man.r - lengthVector
+        // console.log(block, lengthVector)
+        // if (overlap > 0) {   
+        //     WIN.left -= nearVector.x / lengthVector //* speedInfantryNow
+        //     WIN.bottom -= nearVector.y / lengthVector //* speedInfantryNow
+        // }
+    }
+    const circlesArr: TUnit[] = [{x: 4, y: 4, r: 0.25}, {x: 4.5, y: 4, r: 0.25}, {x: 5, y: 4, r: 0.25},{x: 5.5, y: 4, r: 0.25}, {x: 6, y: 4, r: 0.25}, {x: 6.5, y: 4, r: 0.25},{x: 7, y: 4, r: 0.25}]
     const checkAllBlocksInfantry = () => {
+        const arrBlocks: TPoint[][] = [] 
         blocksArray.forEach((block, i) => {
             if (block[0].x >= Math.floor(man.x - 1) && block[0].y >= Math.floor(man.y - 1) && block[2].x <= Math.ceil(man.x + 1) && block[2].y <=  Math.ceil(man.y + 1)) {
                 checkBlockInfantry(block)
+                
+                arrBlocks.push(block)
             }
         })
+        circlesArr.forEach( circle => checkCircleInfantry(circle))
+        console.log(arrBlocks)
+        return arrBlocks
     }
+
+    const checkBlocks = () => {
+        const arrBlock = checkAllBlocksInfantry
+
+    }
+            
 
     const renderScene = (FPS: number) => {
         if (canvas) {
@@ -154,15 +198,16 @@ const GamePage: React.FC = () => {
             canvas.block(blocksArray[1], 'green')
             canvas.block(blocksArray[2], 'green')
             canvas.block(blocksArray[3], 'green')
-
+            canvas.block([{x: 0, y: 0}, {x: 1, y: 0}, {x: 1, y: 60}, {x: 0, y: 60}], 'green')
             canvas.grid()
-
+            canvas.point(4,4,'red', 35)
            
             // moveSceneTank(keyPressed)
             // checkAllBlocksTank()
             // turnTanks(keyPressed)
-            checkAllBlocksInfantry()
+            
             moveSceneInfantry(keyPressed)
+            checkAllBlocksInfantry()
         }
     }
 
