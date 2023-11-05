279ca9d59e9b2ff4c536bac617fd4f5c9e0795b5
vlad32328 <71821947+vlad32328@users.noreply.github.com>
2023-11-05 23:35:20 +0400

    пробный вариант танкового лобби

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,11 @@
 import React from "react";
-import { PageHandler } from "./pages";
 import "./styles/global.css";
-<<<<<<< HEAD
 import { LoadingPage,TankLobbyPage } from "./pages";
-=======
->>>>>>> bc9485f16bdfd76320da271a4c962771d91841cd
 
 const App: React.FC = () => {
    return (
       <div className="app">
-<<<<<<< HEAD
         <TankLobbyPage></TankLobbyPage>
-=======
-         <PageHandler />
->>>>>>> bc9485f16bdfd76320da271a4c962771d91841cd
       </div>
    );
 };
