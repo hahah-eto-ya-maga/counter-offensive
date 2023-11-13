762df52dcdd57b84254ca011b4437a99df05dfab
ek5em <pro-100-dim@mail.ru>
2023-11-13 18:44:13 +0400

    Merge dev task_124

--- client/src/pages/ErrorPage/ErrorPage.tsx
+++ client/src/pages/ErrorPage/ErrorPage.tsx
@@ -1,14 +1,21 @@
 import { FC } from "react";
 import { IError } from "../../modules/Server/types";
+import { Button } from "../../components";
 import "./ErrorPage.css";
 
 const ErrorPage: FC<IError> = ({ code, text }) => {
    return (
       <div className="error-page-wrapper">
-         <div className="error_code">
-            <span id="test-error-code">Error № {code}</span>
+         <div className="error-code-block">
+            <div className="error-code">
+               <span id="test-error-code">Error № {code}</span>
+            </div>
+            <div>
+
+            <Button appearance="primary" id="test-error-back-button">Назад</Button>
+            </div>
          </div>
-         <div className="error_text">
+         <div className="error-text">
             <span id="test-error-text">{text}</span>
          </div>
       </div>
