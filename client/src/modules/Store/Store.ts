b9e48db3e187f7a04dbae73c02d5f340742a5b06
ek5em <pro-100-dim@mail.ru>
2023-11-17 21:44:04 +0400

    [ADD] Добавлена возможность хранить token пользователя.

--- client/src/modules/Store/Store.ts
+++ client/src/modules/Store/Store.ts
@@ -1,3 +1,13 @@
 export default class Store {
-    
+   token: string | null;
+   lobbyHash: string | null;
+   bulletHash: string | null;
+   gameHash: string | null;
+
+   constructor() {
+      this.token = null;
+      this.lobbyHash = null;
+      this.bulletHash = null;
+      this.gameHash = null;
+   }
 }
