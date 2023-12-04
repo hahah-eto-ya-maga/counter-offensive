5e337859302c28b5431903bca182741b47bdccc7
nmoneta <nmoneta20.2@yandex.ru>
2023-12-04 21:44:04 +0400

    [FIX] Исправил баг с рангами в сообщениях

--- server/public/Application/modules/DB.php
+++ server/public/Application/modules/DB.php
@@ -96,11 +96,10 @@ class DB {
     }
 
     function getMessages($userId) {
-        $query = "SELECT u.id AS userId, u.nickname AS nickname, m.text AS text, r.name AS rank_name, m.sendTime AS sendTime
+        $query = "SELECT u.nickname AS nickname, m.text AS text, r.id AS level, r.name AS rank_name, m.sendTime AS sendTime
         FROM messages AS m 
         INNER JOIN users AS u ON m.userId=u.id
-        JOIN gamers AS g ON u.id=g.user_id
-        JOIN ranks AS r ON r.experience<=g.experience
+        JOIN ranks AS r ON r.id=(SELECT MAX(r.id) FROM ranks as r INNER JOIN gamers AS g WHERE r.experience<=g.experience)
         ORDER BY m.sendTime DESC
         LIMIT 30";
         return $this->queryHandlerAll($query, []);
