5060f31738a567d5839c1da304e470862b868c90
nmoneta <nmoneta20.2@yandex.ru>
2023-12-06 23:50:30 +0400

    [ADD] Изменил архитектуру сервера

--- server/public/Application/modules/Chat.php
+++ server/public/Application/modules/Chat.php
@@ -1,37 +1,14 @@
 <?php
+require_once('BaseModule.php');
 
-class Chat
+
+class Chat extends BaseModule 
 {
-    private $db;
 
     function __construct($db)
     {
-        $this->db = $db;
+        parent::__construct($db);
     }
-
-    function v4_UUID()
-    {
-        return sprintf(
-            '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-            // 32 bits for the time_low
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff),
-            // 16 bits for the time_mid
-            mt_rand(0, 0xffff),
-            // 16 bits for the time_hi,
-            mt_rand(0, 0x0fff) | 0x4000,
-
-            // 8 bits and 16 bits for the clk_seq_hi_res,
-            // 8 bits for the clk_seq_low,
-            mt_rand(0, 0x3fff) | 0x8000,
-            // 48 bits for the node
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff)
-        );
-    }
-
-    
 
     public function sendMessage($userId, $message)
     {
--- server/public/Application/modules/Lobby.php
+++ server/public/Application/modules/Lobby.php
@@ -1,12 +1,12 @@
 <?php
-    class Lobby{
+require_once('BaseModule.php');
 
-        protected $db;
+    class Lobby extends BaseModule{
 
         private $lobbyState;
 
         public function __construct($db){
-            $this->db = $db;
+            parent::__construct($db);
             $this->lobbyState = array(
                 "general" => array(
                         "occupied" => false,
@@ -20,24 +20,6 @@
                 "gunner" => false,
                 "infantryRPG" => false
                 );
-        }
-
-
-        function v4_UUID() {
-            return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-              // 32 bits for the time_low
-              mt_rand(0, 0xffff), mt_rand(0, 0xffff),
-              // 16 bits for the time_mid
-              mt_rand(0, 0xffff),
-              // 16 bits for the time_hi,
-              mt_rand(0, 0x0fff) | 0x4000,
-        
-              // 8 bits and 16 bits for the clk_seq_hi_res,
-              // 8 bits for the clk_seq_low,
-              mt_rand(0, 0x3fff) | 0x8000,
-              // 48 bits for the node
-              mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
-            );
         }
 
         function checkTanks($userId)
