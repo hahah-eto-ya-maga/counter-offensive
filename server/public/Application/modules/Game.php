47d89af8a57c3aa5a2d5648edee836c4b6996fc3
Pchelovl <98203135+Pchelovl@users.noreply.github.com>
2023-12-16 07:35:47 +0400

    Сделал shootReg

--- server/public/Application/modules/Game.php
+++ server/public/Application/modules/Game.php
@@ -187,10 +187,36 @@ class Game extends BaseModule
     }
     private function shootReg($id, $x1, $y1, $x2, $y2) {
         foreach($this->gamers as $gamer){
-            $x0 = $gamer->x;
-            $y0 = $gamer->y;
-            if ($x0>$x1 && $x0<$x2 || $x0<$x1 && $x0>$x2){
+            $A = $gamer->x - $x1;
+            $B = $gamer->y - $y1;
+            $C = $x2 - $x1;
+            $D = $y2 - $y1;
+            
+            $dot = $A * $C + $B * $D;
+            $len_sq = $C * $C + $D * $D;
+            $param = -1;
+            if ($len_sq != 0) {
+                    $param = $dot / $len_sq;
+            }
+            $xx = $yy = 0;
+            
+            if ($param < 0) {
+                $xx = $x1;
+                $yy = $y1;
+            } else if ($param > 1) {
+                $xx = $x2;
+                $yy = $y2;
+            } else {
+                $xx = $x1 + $param * $C;
+                $yy = $y1 + $param * $D;
+            }
+            
+            $dx = $gamer->x - $xx;
+            $dy = $gamer->y - $yy;
 
+            if (sqrt($dx * $dx + $dy * $dy)<=0.2){
+                // Сюда запихиваем метод по уменьшению у геймера $gamer->id
+                $this->db->deleteBullet($id);
             }
         }
     }
