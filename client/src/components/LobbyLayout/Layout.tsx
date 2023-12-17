ee8d3a4a76d669fe7121634c6206250d0785f43f
unknown <pro-100-dim@mail.ru>
2023-12-17 11:17:04 +0400

    [FIX] Баг отображения зантости роли.

--- client/src/components/LobbyLayout/Layout.tsx
+++ client/src/components/LobbyLayout/Layout.tsx
@@ -11,96 +11,104 @@ import { tank2, tank3, general } from "../../assets/png";
 import "./Layout.css";
 
 export enum ETank {
-   heavy,
-   middle,
+  heavy,
+  middle,
 }
 
 export const withLayout = (
-   Component: FunctionComponent<{ lobby: ILobby | null }>
+  Component: FunctionComponent<{ lobby: ILobby | null }>
 ) => {
-   return (): JSX.Element => {
-      const [lobby, setLobby] = useState<ILobby | null>(null);
+  return (): JSX.Element => {
+    const [lobby, setLobby] = useState<ILobby>({
+      bannerman: true,
+      general: true,
+      is_alive: false,
+      tanks: {
+        heavyTank: [],
+        middleTank: [],
+      },
+    });
 
-      const server = useContext(ServerContext);
-      const mediator = useContext(MediatorContext);
-      const setRoleHandler = useSetRoleHandler();
-      const navigate = useNavigate();
-      const location = useLocation();
+    const server = useContext(ServerContext);
+    const mediator = useContext(MediatorContext);
+    const setRoleHandler = useSetRoleHandler();
+    const navigate = useNavigate();
+    const location = useLocation();
 
-      const { THROW_TO_GAME } = mediator.getTriggerTypes();
-      const path = location.pathname.split("/")[1];
+    const { THROW_TO_GAME } = mediator.getTriggerTypes();
+    const path = location.pathname.split("/")[1];
 
-      useEffect(() => {
-         const interval = setInterval(async () => {
-            const res = await server.getLobby();
-            if (res && res !== true) {
-               server.STORE.setHash(EHash.lobbyHash, res.lobbyHash);
-               setLobby(res.lobby);
-               res.lobby.is_alive && mediator.get(THROW_TO_GAME);
-            }
-         }, requestDelay.lobby);
-         return () => {
-            server.STORE.setHash(EHash.lobbyHash, null);
-            clearInterval(interval);
-         };
-      }, []);
-
-      const onClickTankLobbyHandler = (switchTo: ETank): void => {
-         const replace: boolean =
-            path === "heavy_tanks" || path === "middle_tanks";
-
-         switch (switchTo) {
-            case ETank.heavy: {
-               return navigate("/heavy_tanks", { replace });
-            }
-            case ETank.middle: {
-               return navigate("/middle_tanks", { replace });
-            }
-         }
+    useEffect(() => {
+      const interval = setInterval(async () => {
+        const res = await server.getLobby();
+        if (res && res !== true) {
+          server.STORE.setHash(EHash.lobbyHash, res.lobbyHash);
+          setLobby(res.lobby);
+          res.lobby.is_alive && mediator.get(THROW_TO_GAME);
+        }
+      }, requestDelay.lobby);
+      return () => {
+        server.STORE.setHash(EHash.lobbyHash, null);
+        clearInterval(interval);
       };
+    }, []);
 
-      return (
-         <div className="lobby_wrapper">
-            <Logo />
-            <div className="lobby_block">
-               <div className={cn("lobby_units_block", "lobby_main_units")}>
-                  <Button
-                     id="test_button_general"
-                     className={cn("general units_item", {
-                        selected_role: !lobby?.general,
-                     })}
-                     appearance="image"
-                     onClick={() => setRoleHandler(EGamerRole.general)}
-                  >
-                     Генерал
-                     <img src={general} alt="General" />
-                  </Button>
-                  <Button
-                     id="test_button_2tank"
-                     className={cn("units_item", {
-                        tank_selected: path === "middle_tanks",
-                     })}
-                     appearance="image"
-                     onClick={() => onClickTankLobbyHandler(ETank.middle)}
-                  >
-                     Двухместный танк
-                     <img src={tank2} alt="Tank_2" />
-                  </Button>
-                  <Button
-                     id="test_button_3tank"
-                     className={cn("units_item", {
-                        tank_selected: path === "heavy_tanks",
-                     })}
-                     appearance="image"
-                     onClick={() => onClickTankLobbyHandler(ETank.heavy)}
-                  >
-                     Трёхместный танк
-                     <img src={tank3} alt="Tank_3" />
-                  </Button>
-               </div>
-               <Component lobby={lobby} />
-            </div>
-         </div>
-      );
-   };
+    const onClickTankLobbyHandler = (switchTo: ETank): void => {
+      const replace: boolean =
+        path === "heavy_tanks" || path === "middle_tanks";
+
+      switch (switchTo) {
+        case ETank.heavy: {
+          return navigate("/heavy_tanks", { replace });
+        }
+        case ETank.middle: {
+          return navigate("/middle_tanks", { replace });
+        }
+      }
+    };
+
+    return (
+      <div className="lobby_wrapper">
+        <Logo />
+        <div className="lobby_block">
+          <div className={cn("lobby_units_block", "lobby_main_units")}>
+            <Button
+              id="test_button_general"
+              className={cn("general units_item", {
+                selected_role: !lobby.general,
+              })}
+              appearance="image"
+              onClick={() => setRoleHandler(EGamerRole.general)}
+            >
+              Генерал
+              <img src={general} alt="General" />
+            </Button>
+            <Button
+              id="test_button_2tank"
+              className={cn("units_item", {
+                tank_selected: path === "middle_tanks",
+              })}
+              appearance="image"
+              onClick={() => onClickTankLobbyHandler(ETank.middle)}
+            >
+              Двухместный танк
+              <img src={tank2} alt="Tank_2" />
+            </Button>
+            <Button
+              id="test_button_3tank"
+              className={cn("units_item", {
+                tank_selected: path === "heavy_tanks",
+              })}
+              appearance="image"
+              onClick={() => onClickTankLobbyHandler(ETank.heavy)}
+            >
+              Трёхместный танк
+              <img src={tank3} alt="Tank_3" />
+            </Button>
+          </div>
+          <Component lobby={lobby} />
+        </div>
+      </div>
+    );
+  };
 };
